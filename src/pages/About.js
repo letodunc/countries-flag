52e008189851c67d64ff54248098bfdb19d18984
Stephen Meyo <meyo79@hotmail.com>
2022-06-06 07:30:46 +0200

    display world countries flag in react app

---
+++ src/pages/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        In culpa nostrud nisi nulla qui adipisicing deserunt officia. Et
+        cupidatat occaecat aliquip veniam eiusmod reprehenderit deserunt culpa
+        nostrud dolore voluptate sit dolore. Irure non in adipisicing fugiat
+        exercitation ut cupidatat cillum eu non consectetur excepteur qui.
+      </p>
+      <br />
+      <p>
+        Nulla et reprehenderit cupidatat ea nostrud incididunt reprehenderit sit
+        enim est dolore aliquip sit. Sint in ea dolor id velit dolor qui eu
+        officia veniam. Aliqua exercitation pariatur et velit id.
+      </p>
+    </div>
+  );
+};
+
+export default About;
